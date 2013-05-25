212147ee11663a30159483255204c8562ce9c005
ae06710 <ae06710@gmail.com>
2013-05-25 12:04:05 +0900

    Impl setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOT_FILES=(.vimrc .tmux.conf .zshrc .gitconfig .gitignore_global)
+DOT_DIRECTORIES=(.vim .zsh-completions)
+
+for file in ${DOT_FILES[@]}
+do
+  if [ -a $HOME/$file ]; then
+    echo "$file file is existed."
+  else
+    ln -s $HOME/dotfiles/$file $HOME/$file
+  fi
+done
+
+for directory in ${DOT_DIRECTORIES[@]}
+do
+  if [ -d $HOME/$directory ]; then
+    echo "$directory directory is existed."
+  else
+    ln -s $HOME/dotfiles/$directory $HOME/$directory
+  fi
+done
