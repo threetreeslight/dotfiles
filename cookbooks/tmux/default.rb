d62f1f501bf4089e020c1a1b1e72d2660452a7fe
threetreeslight <ae06710@gmail.com>
2017-12-31 18:56:30 +0900

    add tmuxinator settings

--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -1,7 +1,14 @@
 # reattach-to-user-namespace: use tmux to clipbord
-%w( tmux tmux-cssh reattach-to-user-namespace ).each do |n|
+%w(
+  tmux
+  tmux-cssh
+  tmuxinator-completion
+  reattach-to-user-namespace
+).each do |n|
   package n
 end
 
 dotfile '.tmux.conf'
+
+gem_package 'tmuxinator'
 dotfile '.tmuxinator'
