b90de5e90fa666bc86e9b6b8d9951aa802043843
threetreeslight <ae06710@gmail.com>
2018-01-07 16:25:50 +0900

    fix(git: use github/gitignore/Global settings to use gitignore global

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -8,11 +8,30 @@ package 'hub' # github cli
   dotfile n
 end
 
+# ignore global
+github_gitignore_dir = "#{ENV['HOME']}/src/github.com/github/gitignore"
+directory github_gitignore_dir
+
+git github_gitignore_dir do
+  repository 'https://github.com/github/gitignore.git'
+end
+
+git_config_dir = "#{ENV['HOME']}/.config/git"
+git_config_ignore_path = "#{git_config_dir}/ignore"
+directory git_config_dir
+
+execute 'symlink $HOME/.config/git/ignore dir' do
+  not_if "test -L #{git_config_ignore_path} && echo true"
+  command "ln -s #{github_gitignore_dir}/Global #{git_config_ignore_path}"
+end
+
 execute 'symlink diff-highlight' do
   not_if 'test -L /usr/local/bin/diff-highlight && echo true'
   command <<-CMD
   find `brew --prefix` -name diff-highlight | awk 'NR == 2 { print $0 }' | xargs -I{} sudo ln -s {} /usr/local/bin/
 CMD
 end
+
+
 
 
